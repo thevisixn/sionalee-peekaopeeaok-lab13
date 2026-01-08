87e50888cf7b4d4e1f951a99b5e32e9f3261c71f
visionalee <flook.xtnt@gmail.com>
2026-01-09 00:40:17 +0700

    (2)

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,36 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool A[][M])
+{
+    cout << "------------------------------------------------------------------------\n";
+    for (int i = 0; i < N; i++){
+        cout << "|";
+        for (int j = 0; j < M; j++)
+        {
+            if (A[i][j] == true)
+            {
+                cout << "*";
+            }
+            else {
+                cout << " ";
+            }
+        }
+        cout << "|\n";
+    }
+    cout << "------------------------------------------------------------------------\n";
+}
+
+void updateImage(bool A[][M],int size,int x_pos,int y_pos){
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < M; j++)
+        {
+            if (sqrt(pow(i-x_pos,2)+pow(j-y_pos,2)) <= size-1)
+            {
+                A[i][j] = true;
+            }
+            
+        }
+    }
+}
